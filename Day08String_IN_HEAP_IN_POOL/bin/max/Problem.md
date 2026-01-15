426bc26bb3ae25ce329fb48fc55f1e6db5dc4f22
Satya Raghav <satyaraghav13@gmail.com>
2026-01-15 16:05:43 +0530

    String in Heap and Pool

---
+++ Day08String_IN_HEAP_IN_POOL/bin/max/Problem.md
@@ -0,0 +1,11 @@
+## Problem
+Write a Core Java program to demonstrate the use of common
+String methods such as `equals()`, `toLowerCase()`, `charAt()`,
+and `substring()`.
+
+## Input
+``java
+String s  = new String("India");
+String s1 = new String("india");
+String s2 = "inDia";
+String s3 = "indiabbb";
---
+++ Day08String_IN_HEAP_IN_POOL/src/max/Solution01.java
@@ -0,0 +1,43 @@
+package max;
+
+public class Solution01 {
+public static void main(String[] args) {
+	String s = new String("india");
+	String s1 = new String("india");
+	String s2="india";
+	String s3="india";
+	System.out.println(s3.equals(s1));
+	System.out.println(s == s1);
+//	System.out.println(s2.toLowerCase());
+//	System.out.println(s.toUpperCase());
+//	System.out.println(s3.charAt(1));
+//	System.out.println(s3.substring(2,3));
+//	
+//	char w[]={'a','0','m'};//a 97 A 65
+//	
+//	for(int z: w)
+//	{
+//		System.out.println(z);
+//	}
+//	
+//	
+//	
+//	String ss = new String(w);
+//	
+//	System.out.println(ss);
+//	char[] r = s3.toCharArray();
+//	
+//	 for( char  p: r)
+//	 {
+//		 System.out.println(p);
+//	 }
+//	System.out.println("-------");
+//	
+//	for(int i=0;i<r.length;)
+//	{
+//		System.out.println(r[i]);
+//		i++;
+//	}
+//	
+}
+}
