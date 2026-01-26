c851f1a6a93c9128d1ce9c88ec51af0a1cee706b
Satya Raghav <satyaraghav13@gmail.com>
2026-01-26 16:13:27 +0530

    SecondHighest

---
+++ Day18_Collection_Framework_Queeee/src/Solution01.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Solution01 {
+
+    public static void main(String[] args) {
+
+        List<Integer> salaries = new ArrayList<>(
+                Arrays.asList(50000, 400000, 60000, 55000, 60000)
+        );
+
+        // Remove duplicates
+        salaries = new ArrayList<>(salaries.stream().distinct().toList());
+
+        // Sort in descending order
+        Collections.sort(salaries, Collections.reverseOrder());
+
+        // Get second highest
+        if (salaries.size() >= 2) {
+            System.out.println("Second Highest Salary: " + salaries.get(1));
+        } else {
+            System.out.println("Not enough salary data");
+        }
+    }
+}
+
