5e79becd835207d36ef070b928e75819e869b389
Satya Raghav <satyaraghav13@gmail.com>
2026-02-09 20:04:24 +0530

    public class Solution01 {

--- Day31_Encapsulation/src/Solution01.java
+++ Day31_Encapsulation/src/Solution01.java
@@ -1,4 +1,33 @@
+class Student {
+    private String name;
+    private int age;
+
+    // Getter
+    public String getName() {
+        return name;
+    }
+
+    // Setter
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
 
 public class Solution01 {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.setName("Satya");
+        s.setAge(22);
 
+        System.out.println("Name: " + s.getName());
+        System.out.println("Age: " + s.getAge());
+    }
 }
---
+++ Day32_Reverse_Without_Inbuild_Function/src/Solution01.java
@@ -0,0 +1,13 @@
+public class Solution01 {
+    public static void main(String[] args) {
+        String str = "Java";
+        String reversed = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed = reversed + str.charAt(i);
+        }
+
+        System.out.println("Original String: " + str);
+        System.out.println("Reversed String: " + reversed);
+    }
+}
